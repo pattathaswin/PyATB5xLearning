053c8fdfb841a55c43e418785d9179d72f66d391
pattathaswin <pattathaswin@gmail.com>
2024-11-11 20:57:02 +0530

    09 Nov

---
+++ Nov/ex_09112024_Literals_Variables/Lab0017.py
@@ -0,0 +1,9 @@
+firstname = "Aswin"
+lastname = "kumar"
+
+fullname =  (firstname + " " + lastname)
+
+print (fullname)
+
+print ("Hello\tWorld")
+print("Hello\nWorld")
---
+++ Nov/ex_09112024_Literals_Variables/Lab0018.py
@@ -0,0 +1,12 @@
+firstname = "Aswin"
+lastname = "kumar"
+
+fullname =  (firstname + " " + lastname)
+
+print (fullname)
+
+print ("Hello\tWorld")
+print("Hello\nWorld")
+
+print (r"Hello\tWorld")
+print(r"Hello\nWorld")
